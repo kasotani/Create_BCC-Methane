012a73a6efbea17ed92d7f4d16132d661487c619
kasotani <100916614+kasotani@users.noreply.github.com>
2022-10-14 00:11:07 +0900

    Update BCC.py

--- make_bcc/BCC.py
+++ make_bcc/BCC.py
@@ -11,58 +11,28 @@ print(x**3)
 for i in range(2*x):
     for j in range(int(x/2)):
         for k in range(int(x/2)):
-            print(" "*(5-len(str(met)))+f"{met}MET", " C", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i, 3)))))+str('{:.3f}'.format(round(2*a*i, 3))),\
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j, 3)))))+str('{:.3f}'.format(round(2*a*j, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k, 3)))))+str('{:.3f}'.format(round(2*a*k, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i, 3)), '{:8.3f}'.format(round(2*a*j, 3)), '{:8.3f}'.format(round(2*a*k, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+b, 3)))))+str('{:.3f}'.format(round(2*a*i+b, 3))),\
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+b, 3)))))+str('{:.3f}'.format(round(2*a*j+b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+b, 3)))))+str('{:.3f}'.format(round(2*a*k+b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+b, 3)), '{:8.3f}'.format(round(2*a*j+b, 3)), '{:8.3f}'.format(round(2*a*k+b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+b, 3)))))+str('{:.3f}'.format(round(2*a*i+b, 3))),\
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j-b, 3)))))+str('{:.3f}'.format(round(2*a*j-b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k-b, 3)))))+str('{:.3f}'.format(round(2*a*k-b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+b, 3)), '{:8.3f}'.format(round(2*a*j-b, 3)), '{:8.3f}'.format(round(2*a*k-b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i-b, 3)))))+str('{:.3f}'.format(round(2*a*i-b, 3))),\
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+b, 3)))))+str('{:.3f}'.format(round(2*a*j+b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k-b, 3)))))+str('{:.3f}'.format(round(2*a*k-b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i-b, 3)), '{:8.3f}'.format(round(2*a*j+b, 3)), '{:8.3f}'.format(round(2*a*k-b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i-b, 3)))))+str('{:.3f}'.format(round(2*a*i-b, 3))),\
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j-b, 3)))))+str('{:.3f}'.format(round(2*a*j-b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+b, 3)))))+str('{:.3f}'.format(round(2*a*k+b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i-b, 3)), '{:8.3f}'.format(round(2*a*j-b, 3)), '{:8.3f}'.format(round(2*a*k+b, 3)))
             atom += 1
             met += 1
     for j in range(int(x/2)):
         for k in range(int(x/2)):
-            print(" "*(5-len(str(met)))+f"{met}MET", " C", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+a, 3)))))+str('{:.3f}'.format(round(2*a*i+a, 3))), \
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+a, 3)))))+str('{:.3f}'.format(round(2*a*j+a, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+a, 3)))))+str('{:.3f}'.format(round(2*a*k+a, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+a, 3)), '{:8.3f}'.format(round(2*a*j+a, 3)), '{:8.3f}'.format(round(2*a*k+a, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+a+b, 3)))))+str('{:.3f}'.format(round(2*a*i+a+b, 3))), \
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+a+b, 3)))))+str('{:.3f}'.format(round(2*a*j+a+b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+a+b, 3)))))+str('{:.3f}'.format(round(2*a*k+a+b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+a+b, 3)), '{:8.3f}'.format(round(2*a*j+a+b, 3)), '{:8.3f}'.format(round(2*a*k+a+b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+a+b, 3)))))+str('{:.3f}'.format(round(2*a*i+a+b, 3))), \
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+a-b, 3)))))+str('{:.3f}'.format(round(2*a*j+a-b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+a-b, 3)))))+str('{:.3f}'.format(round(2*a*k+a-b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+a+b, 3)), '{:8.3f}'.format(round(2*a*j+a-b, 3)), '{:8.3f}'.format(round(2*a*k+a-b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+a-b, 3)))))+str('{:.3f}'.format(round(2*a*i+a-b, 3))), \
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+a+b, 3)))))+str('{:.3f}'.format(round(2*a*j+a+b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+a-b, 3)))))+str('{:.3f}'.format(round(2*a*k+a-b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+a-b, 3)), '{:8.3f}'.format(round(2*a*j+a+b, 3)), '{:8.3f}'.format(round(2*a*k+a-b, 3)))
             atom += 1
-            print(" "*(5-len(str(met)))+f"{met}MET", " H", " "*(6-len(str(atom)))+str(atom),\
-                " "*(9-len(str('{:.3f}'.format(round(2*a*i+a-b, 3)))))+str('{:.3f}'.format(round(2*a*i+a-b, 3))), \
-                    " "*(8-len(str('{:.3f}'.format(round(2*a*j+a-b, 3)))))+str('{:.3f}'.format(round(2*a*j+a-b, 3))),\
-                        " "*(8-len(str('{:.3f}'.format(round(2*a*k+a+b, 3)))))+str('{:.3f}'.format(round(2*a*k+a+b, 3))))
+            print(f"{'{:5d}'.format(met)}MET", " H", '{:6d}'.format(atom), '{:9.3f}'.format(round(2*a*i+a-b, 3)), '{:8.3f}'.format(round(2*a*j+a-b, 3)), '{:8.3f}'.format(round(2*a*k+a+b, 3)))
             atom += 1
             met += 1
 print(round(4*x*a, 5), round(x*a, 5), round(x*a, 5))
