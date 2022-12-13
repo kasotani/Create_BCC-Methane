faf87c40eaf711482a6061c15188270ac902000c
kasotani <100916614+kasotani@users.noreply.github.com>
2022-12-13 12:55:25 +0900

    Update BCC.py

--- make_bcc/BCC.py
+++ make_bcc/BCC.py
@@ -1,4 +1,5 @@
-rcc = 0.900  # C_C間距離(nm)
+# rcc = 0.900  # C_C間距離(nm)
+rcc = 1.500
 rch = 0.109  # C_H間距離(nm)
 a = rcc/3**0.5
 b = rch/3**0.5
@@ -11,28 +12,28 @@ print(x**3*5)
 for i in range(2*x):
     for j in range(int(x/2)):
         for k in range(int(x/2)):
-            print(f"{'{:5d}'.format(met)}MET", "     C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i, 3)), '{:7.3f}'.format(round(2*a*j, 3)), '{:7.3f}'.format(round(2*a*k, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i, 3)), '{:7.3f}'.format(round(2*a*j, 3)), '{:7.3f}'.format(round(2*a*k, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+b, 3)), '{:7.3f}'.format(round(2*a*j+b, 3)), '{:7.3f}'.format(round(2*a*k+b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+b, 3)), '{:7.3f}'.format(round(2*a*j+b, 3)), '{:7.3f}'.format(round(2*a*k+b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+b, 3)), '{:7.3f}'.format(round(2*a*j-b, 3)), '{:7.3f}'.format(round(2*a*k-b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+b, 3)), '{:7.3f}'.format(round(2*a*j-b, 3)), '{:7.3f}'.format(round(2*a*k-b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i-b, 3)), '{:7.3f}'.format(round(2*a*j+b, 3)), '{:7.3f}'.format(round(2*a*k-b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i-b, 3)), '{:7.3f}'.format(round(2*a*j+b, 3)), '{:7.3f}'.format(round(2*a*k-b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i-b, 3)), '{:7.3f}'.format(round(2*a*j-b, 3)), '{:7.3f}'.format(round(2*a*k+b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i-b, 3)), '{:7.3f}'.format(round(2*a*j-b, 3)), '{:7.3f}'.format(round(2*a*k+b, 3)))
             atom += 1
             met += 1
     for j in range(int(x/2)):
         for k in range(int(x/2)):
-            print(f"{'{:5d}'.format(met)}MET", "     C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a, 3)), '{:7.3f}'.format(round(2*a*j+a, 3)), '{:7.3f}'.format(round(2*a*k+a, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a, 3)), '{:7.3f}'.format(round(2*a*j+a, 3)), '{:7.3f}'.format(round(2*a*k+a, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a+b, 3)), '{:7.3f}'.format(round(2*a*j+a+b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a+b, 3)), '{:7.3f}'.format(round(2*a*j+a+b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a+b, 3)), '{:7.3f}'.format(round(2*a*j+a-b, 3)), '{:7.3f}'.format(round(2*a*k+a-b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a+b, 3)), '{:7.3f}'.format(round(2*a*j+a-b, 3)), '{:7.3f}'.format(round(2*a*k+a-b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a-b, 3)), '{:7.3f}'.format(round(2*a*j+a+b, 3)), '{:7.3f}'.format(round(2*a*k+a-b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a-b, 3)), '{:7.3f}'.format(round(2*a*j+a+b, 3)), '{:7.3f}'.format(round(2*a*k+a-b, 3)))
             atom += 1
-            print(f"{'{:5d}'.format(met)}MET", "     H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a-b, 3)), '{:7.3f}'.format(round(2*a*j+a-b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
+            print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a-b, 3)), '{:7.3f}'.format(round(2*a*j+a-b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
             atom += 1
             met += 1
 print(round(4*x*a, 5), round(x*a, 5), round(x*a, 5))
