dca98e208411bf5b436ca7133a98156158994916
kasotani <100916614+kasotani@users.noreply.github.com>
2023-02-05 08:37:16 +0900

    Update BCC.py

--- make_bcc/BCC.py
+++ make_bcc/BCC.py
@@ -9,9 +9,9 @@ atom = 1 # 原子の通し番号
 
 print("methane")
 print(x**3*5)
-for i in range(2*x):
+for i in range(int(x/2)):
     for j in range(int(x/2)):
-        for k in range(int(x/2)):
+        for k in range(int(x*2)):
             print(f"{'{:5d}'.format(met)}  MET    C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i, 3)), '{:7.3f}'.format(round(2*a*j, 3)), '{:7.3f}'.format(round(2*a*k, 3)))
             atom += 1
             print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+b, 3)), '{:7.3f}'.format(round(2*a*j+b, 3)), '{:7.3f}'.format(round(2*a*k+b, 3)))
@@ -24,7 +24,7 @@ for i in range(2*x):
             atom += 1
             met += 1
     for j in range(int(x/2)):
-        for k in range(int(x/2)):
+        for k in range(int(x*2)):
             print(f"{'{:5d}'.format(met)}  MET    C"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a, 3)), '{:7.3f}'.format(round(2*a*j+a, 3)), '{:7.3f}'.format(round(2*a*k+a, 3)))
             atom += 1
             print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a+b, 3)), '{:7.3f}'.format(round(2*a*j+a+b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
@@ -36,4 +36,4 @@ for i in range(2*x):
             print(f"{'{:5d}'.format(met)}  MET    H"+'{:5d}'.format(atom), '{:7.3f}'.format(round(2*a*i+a-b, 3)), '{:7.3f}'.format(round(2*a*j+a-b, 3)), '{:7.3f}'.format(round(2*a*k+a+b, 3)))
             atom += 1
             met += 1
-print(round(4*x*a, 5), round(x*a, 5), round(x*a, 5))
+print(round(x*a, 5), round(x*a, 5), round(4*x*a, 5))
